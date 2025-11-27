810043902b41c81b1d488c81660699dde38f1991
Srujan <157562571+Srujanbj08@users.noreply.github.com>
2025-11-27 13:38:03 +0530

    Update App.jsx
    update the frontend js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
     try {
       const cleanSmiles = smiles.trim().split(/\s+/)[0]
 
-      const response = await fetch('http://127.0.0.1:8000/predict', {
+      const response = await fetch('https://molformer.onrender.com/predict', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ smiles: cleanSmiles })
